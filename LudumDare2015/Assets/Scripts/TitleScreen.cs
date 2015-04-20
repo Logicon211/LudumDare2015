4fa66f4b1ae3aab86308e5a30cc363ee21962242
KingOfBraves <chadifferus@gmail.com>
2015-04-19 22:16:33 -0300

    Bondage Party

---
+++ LudumDare2015/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreen : MonoBehaviour {
+
+	void Update(){
+		if(Input.GetKeyDown("return")){
+			Debug.Log ("no");
+			Application.LoadLevel("Scene1");
+
+		}
+	}
+
+}
